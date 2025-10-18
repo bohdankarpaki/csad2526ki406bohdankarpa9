5ff9d50ba773d0a56406a3af0cc9040405bc7ab6
karpa <karpabohdan2@gmail.com>
2025-10-18 18:48:20 +0300

    563

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main() {
     int result = add(5, 3);
     std::cout << "Hello, World!" << std::endl;
     std::cout << "The result of 5 + 3 is: " << result << std::endl;
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get(); // Чекає, поки користувач натисне Enter
     return 0;
 }
